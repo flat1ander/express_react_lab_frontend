2a30d03c864a264fe9f078510de09bf9d5f155b2
flat1ander <thematthewlane@gmail.com>
2023-05-08 14:54:47 -0400

    Added info to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,15 @@
 function Footer(props) {
-    return <h1>Footer</h1>
+    return(
+<div className='Footer'>
+
+  <p>Copyright: Matt Lane 2023</p>
+  <p><a href="mailto:thematthewlane@gmail.com">Email Matt</a></p>
+  <a href="https://www.linkedin.com/in/matt-lane-91500366/">
+    <img src="https://i.imgur.com/C3aOPHj.png" alt="IOG Linkedin"/>
+    </a>
+
+</div>
+)
 }
 
 export default Footer;
